2044ee602b6f5d78129d83c7445f4ccc042faeb3
Prabal Pathak <prabal01pathak@gmail.com>
2021-10-06 22:10:45 +0530

    move r files

--- script.js
+++
@@ -1,8 +0,0 @@
-const menuIcon = document.querySelector('.menu-icon');
-const headerLinks = document.querySelector('.header-links');
-
-menuIcon.addEventListener('click', () => {
-    headerLinks.classList.toggle('nav-active');
-    menuIcon.classList.toggle('icon-opacity');
-
-});
